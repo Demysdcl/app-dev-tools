5bf41054ac511d5a5516eccdb3d097c9ba2b7ce1
demyslima <demys.lima@contasimples.com>
2022-12-06 12:06:38 -0300

    feat: montrando spacing names

---
+++ scripts/spacingNames.js
@@ -0,0 +1,18 @@
+const spacingNames = {
+  none: 0,
+  quarck: 4,
+  nano: 8,
+  xxxs: 16,
+  xxs: 24,
+  xs: 32,
+  sm: 40,
+  md: 48,
+  lg: 56,
+  xl: 64,
+  xxl: 80,
+  xxxl: 112,
+  huge: 128,
+  giant: 160,
+};
+
+console.log(spacingNames);
