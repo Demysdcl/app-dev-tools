8a273d057d6c91e7829d0053f8157e4c2647c89a
demyslima <demys.lima@contasimples.com>
2023-06-23 16:42:34 -0300

    feat: removendo dependencia de tokens

--- scripts/getColors.js
+++ scripts/getColors.js
@@ -1,9 +1,61 @@
-const { getTheme } = require('@contasimples/simples-tokens');
-
-const { palette } = getTheme({
-  palette: 'default',
-  environment: 'app',
-});
+const palette = {
+  primary: {
+    main: '#153D3B',
+    light: '#446462',
+  },
+  secondary: {
+    main: '#65CA74',
+    light: '#E0F4E3',
+  },
+  tertiary: {
+    main: '#AFE150',
+    light: '#EFF9DC',
+  },
+  neutral: {
+    darker: '#0D0D0D',
+    dark: '#999999',
+    medium: '#E5E5E5',
+    light: '#F3F3F3',
+    lighter: '#FFFFFF',
+  },
+  support: {
+    dark: {
+      blue: '#347FC4',
+      green: '#AFE150',
+      purple: '#7D6B91',
+      orange: '#EE964B',
+      turquoise: '#4ECDC4',
+      pink: '#FF5B83',
+      grey: '#67809F',
+    },
+    light: {
+      blue: '#D6E5F3',
+      green: '#EFF9DC',
+      purple: '#E5E1E9',
+      orange: '#FCEADB',
+      turquoise: '#EEFAFA',
+      pink: '#FFEFF3',
+      grey: '#F0F3F6',
+    },
+  },
+  feedback: {
+    error: {
+      main: '#7A2D25',
+      medium: '#E74C3C',
+      light: '#FADBD8',
+    },
+    alert: {
+      main: '#7F680D',
+      medium: '#F1C40D',
+      light: '#FCF3CF',
+    },
+    positive: {
+      main: '#396B40',
+      medium: '#65CA74',
+      light: '#E0F4E3',
+    },
+  },
+};
 
 const createKey = (key1, key2) => key1 + '.' + key2;
 
